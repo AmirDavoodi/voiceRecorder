eee8633a1d64fce5d62c2513353be50263c4d789
Amirehsan Davoodi <amirehsan.davoodi@gmail.com>
2022-07-20 23:45:38 +0430

    handle close window event for mac os

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
+// Main process
 const {app, BrowserWindow} = require('electron');
 
-
 function createWindow() {
+    // Browser Window <- Renderer Process
     const win = new BrowserWindow({
-        width: 800,
-        height: 600,
+        width: 1200,
+        height: 800,
         backgroundColor: "white",
         webPreferences:{
             nodeIntegration: true
@@ -12,6 +13,21 @@ function createWindow() {
     });
 
     win.loadFile('index.html')
+    win.webContents.openDevTools();
 }
 
-app.whenReady().then(createWindow)
+app.whenReady().then(createWindow);
+
+// Do not close the application on MAC OS by closing the window
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+// Open new window if there is no open window
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+});
